c772988f1a96309e157d6371289f283b517eec73
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-08-07 17:00:48 +0100

    Merge branch 'master' of https://github.com/theodi/orpi-analysis
    Conflicts:
    main.R

--- data-exploration.R
+++ data-exploration.R
@@ -3,7 +3,7 @@
 setwd('~/git/orpi-analysis')
 
 # List of packages for session
-packages <- c('dplyr', 'ggplot2', 'Hmisc')
+packages <- c('dplyr', 'ggplot2', 'Hmisc', 'lubridate')
 
 # Install CRAN packages (if not already installed)
 .inst <- packages %in% installed.packages()
@@ -15,8 +15,9 @@ sapply(packages, require, character.only = TRUE)
 # Options
 theme_set(theme_minimal(base_family = "Helvetica Neue"))
 options(stringsAsFactors = FALSE)
-options("scipen" = 100, "digits" = 4) # no scientific notation
+options("scipen" = 100, "digits" = 4) # less scientific notation
 # options(digits.secs = 0)
+source('/Users/Ulrich/git/R-projects/ODI-colours.R')
 
 # Functions
 source('functions.r')
@@ -28,7 +29,7 @@ all <- readRDS('data/data-2014-08-04.RData')
 # Clean with script
 all <- raildata.clean(all)
 
-# I'm using DPLYR
+# I'm using DPLYR << apparently not
 all <- tbl_df(all)
 
 #------------------------------------------------
@@ -54,10 +55,15 @@ describe(all[all$body.variation_status %in% 'LATE', 'body.timetable_variation'])
 ggplot(data = all[all$body.timetable_variation > 0, ]) + geom_boxplot(aes(x = body.variation_status, y = body.timetable_variation)) + coord_flip()
 table(all[all$body.timetable_variation <= 10, 'body.timetable_variation'], all[all$body.timetable_variation <= 10, 'body.variation_status'])
 
+ggplot(data = all, aes(x = hour(body.actual_timestamp))) + geom_bar(binwidth = 1, color = 'white', fill = odi_dBlue, origin = -0.5) +
+  scale_x_continuous(breaks = seq(0, 24, 2))
 
 #------------------------------------------------
-delayed_final_dest <- test[(test$body.planned_event_type == "DESTINATION") & (test$body.variation_status == "LATE"), ]
-delayed <- test[test$body.variation_status %in% "LATE", ]
+delayed_final_dest <- all[(all$body.planned_event_type == "DESTINATION") & (all$body.variation_status == "LATE"), ]
+delayed <- all[all$body.variation_status %in% "LATE", ]
+
+
+
 
 #------------------------------------------------
 test <- row.sample(all, 10000)
@@ -66,10 +72,34 @@ test <- row.sample(all, 10000)
 test[, 'time_diff'] <- as.numeric(test[, 'body.actual_timestamp'] - test[, 'body.gbtt_timestamp'])/60
 
 ggplot(data = test) + geom_point(aes(x = time_diff, y = body.timetable_variation), shape = 1) + 
-  facet_wrap(~ body.variation_status) + ylim(0, 50)
+  facet_wrap(~ body.variation_status, nrow = 1) + ylim(0, 50) + xlim(-50, 50)
 
 # Correlations
 cor(test[test$body.variation_status %in% 'LATE', c('time_diff', 'body.timetable_variation')], use = "complete.obs")
 cor(test[test$body.variation_status %in% 'EARLY', c('time_diff', 'body.timetable_variation')], use = "complete.obs")
+
+table(test[, 'time_diff'] - test[, 'body.timetable_variation'])
+dotplot(table(test[, 'time_diff'] - test[, 'body.timetable_variation']), horizontal = F)
+
+#------------------------------------------------
+#--------------- Potential metrics --------------
+
+## Percent delayed
+pct_delayed <- nrow(all[all$body.variation_status %in% "LATE", ]) / nrow(all) # Make sure there are no empty rows etc.
+format.pct(pct_delayed)
+
+## Average minutes
+mean_delayed <- mean(delayed[, 'body.timetable_variation'])
+median_delayed <- median(delayed[, 'body.timetable_variation'])
+format.min(mean_delayed)
+format.min(median_delayed)
+
+## Average minutes for > 1 min delay
+mean_delayed_more1 <- mean(delayed[delayed[, 'body.timetable_variation'] > 1, 'body.timetable_variation'])
+# mean(filter(delayed, body.timetable_variation > 1)$body.timetable_variation)
+median_delayed_more1 <- median(delayed[delayed[, 'body.timetable_variation'] > 1, 'body.timetable_variation'])
+format.min(mean_delayed_more1)
+format.min(median_delayed_more1)
+
 
 
--- main.R
+++ main.R
@@ -11,9 +11,10 @@ AWS_BUCKET_NAME <- "orpi-nrod-store"
 # generated to stderr: this is caused by s3cmd not managing correctly
 # the MD5 of multipart uploads 
 download_data <- function (year = format((Sys.Date() - 1), "%Y"), month = format((Sys.Date() - 1), "%m"), day = format(Sys.Date() - 1, "%d")) {
-    path <- paste0("s3://", AWS_BUCKET_NAME, "/", year, "/", month, "/", day, "/")
+    path <- paste0("s3://", AWS_BUCKET_NAME, "/", formatC(year, width=4, flag="0"), "/", formatC(month, width=2, flag="0"), "/", formatC(day, width=2, flag="0"), "/")
     grep_string <- paste0("^", path , "arrivals_", year, formatC(month, width=2, flag="0"), formatC(day, width=2, flag="0"))
     s3cmd_command <- paste0("/usr/local/bin/s3cmd ls ", path)
+    print(s3cmd_command)
     available_files <- read.table(pipe(s3cmd_command), header = F, sep="", colClasses = "character")  
     available_files <- grep(grep_string, available_files[, ncol(available_files)], value = TRUE) 
     results <- data.frame();
@@ -27,7 +28,14 @@ download_data <- function (year = format((Sys.Date() - 1), "%Y"), month = format
 # examples
 
 # how many train services we recorded yesterday?
-all_arrivals_yesterday <- download_data()
+<<<<<<< HEAD
+<<<<<<< HEAD
+# all_arrivals_yesterday <- download_data()
+=======
+>>>>>>> FETCH_HEAD
+=======
+>>>>>>> FETCH_HEAD
+all_arrivals_yesterday <- download_data(2014, 8, 4)
 length(unique(all_arrivals_yesterday$body.train_id))
 
 # what % were delayed at the final destination? note that there may be more
