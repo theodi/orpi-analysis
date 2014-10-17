732cea6b53aab6e01bed3ca0b62723ef243502be
ulrich <ulrich.atz@theodi.org>
2014-10-17 10:17:23 +0100

    Multiplot

--- daily-summary-stats.R
+++ daily-summary-stats.R
@@ -6,6 +6,7 @@ load("all_days_ranking_13_aug_30_sep.RData")
 # List of packages for session
 packages <- c('ggplot2', 'ggthemes', 'scales')
 sapply(packages, require, character.only = TRUE)
+source("multiplot-function.R")
 
 #------------------------------------------------
 
@@ -45,7 +46,42 @@ ggplot(rail, aes(x = date, y = average_delay)) +
 ggsave("graphics/average-delay-per-day.png", width = 16, height = 4)
 
 
+# Short graphs for comparison
 
 
+p1 <- ggplot(rail, aes(x = date, y = total_lost_minutes / 1000, fill = factor(weekends))) + 
+  geom_bar(stat = "identity") + geom_hline(yintercept = seq(3000, 9000, 3000), col = "white") +
+  xlab("") + ylab("Gross lost minutes in thousands") + scale_y_continuous(labels = comma) +  
+  coord_cartesian(xlim = c(as.Date("2014-09-18") - 0.5, as.Date("2014-09-22") + 0.5)) + 
+  scale_fill_manual(values = c("1" = odi_red, "0" = odi_orange), guide = 'none') +
+  scale_x_date(breaks = "1 day", labels = date_format("%d %b")) +
+  theme(panel.grid.minor = element_blank(), axis.ticks.y = element_blank(), axis.text.x = element_text(angle = 90, vjust = 0.5))
+
+p2 <- ggplot(rail, aes(x = date, y = average_delay)) + 
+  geom_point(colour = odi_dPink, size = 3) + geom_ribbon(ymin = 5, ymax = max_delay + 1, fill = odi_red, alpha = 0.15) +
+  xlab("") + ylab("Average delay in minutes") + ylim(3.5, 7) +
+  coord_cartesian(xlim = c(as.Date("2014-09-18") - 0.5, as.Date("2014-09-22") + 0.5)) + 
+  scale_x_date(breaks = "1 day", labels = date_format("%d %b")) +
+  theme(panel.grid.minor = element_blank(), axis.ticks.y = element_blank(), axis.text.x = element_text(angle = 90, vjust = 0.5))
+
+# ggsave doesn't work here
+png("graphics/compare-selected-days-delays.png", width = 6, height = 4, units = "in", res = 300)
+multiplot(p1, p2, cols = 2)
+dev.off()
+
+# CORRELATION
+
+cor(rail$average_delay, rail$total_lost_minutes)
+
+ggplot(rail, aes(average_delay, total_lost_minutes / 1000, shape = factor(weekends))) + 
+  geom_point(colour = odi_turquoise, size = 5) + 
+  xlab("Average delay in minutes") +
+  ylab("Gross lost minutes in thousands") + scale_y_continuous(labels = comma, limits = c(0, NA))  +
+  scale_shape_manual(values = c("1" = 15, "0" = 19), guide = 'none') +
+  theme(axis.text = element_text(size = 20), axis.title = element_text(size = 20))
+ggsave("graphics/correlation-delay-lost-min.png", width = 12, height = 8)
+
+
+  
 
 
